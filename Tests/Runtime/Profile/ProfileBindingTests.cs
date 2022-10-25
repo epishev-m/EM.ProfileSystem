d6275690f5e59351dc2026ff844341ed98be9622
Maxim Epishev <m.epishev@rj-games.com>
2022-10-25 20:14:51 +0300

    Added ProfileBindingTests

---
+++ Tests/Runtime/Profile/ProfileBindingTests.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using EM.Foundation;
+using EM.Profile;
+using NUnit.Framework;
+
+internal sealed class ProfileBindingTests
+{
+	#region Constructor
+	
+	[Test]
+	public void ProfileBinding_Constructor_Exception()
+	{
+		// Act
+		var actual = false;
+
+		try
+		{
+			var unused = new ProfileBinding(null, null, null);
+		}
+		catch (ArgumentNullException)
+		{
+			actual = true;
+		}
+
+		//Assert
+		Assert.IsTrue(actual);
+	}
+	
+	#endregion
+	
+	#region LifeTime
+
+	[Test]
+	public void ProfileBinding_InGlobal()
+	{
+		// Arrange
+		var key = typeof(Test);
+		var binding = new ProfileBinding(key, null, null);
+
+		// Act
+		binding.InGlobal();
+		var actual = binding.LifeTime;
+
+		// Assert
+		Assert.AreEqual(LifeTime.Global, actual);
+	}
+
+	[Test]
+	public void ProfileBinding_InGlobal_IsInvalidOperationException()
+	{
+		// Arrange
+		var actual = false;
+		var key = typeof(Test);
+		var binding = new ProfileBinding(key, null, null);
+		binding.InGlobal();
+
+		// Act
+		try
+		{
+			binding.InGlobal();
+		}
+		catch (InvalidOperationException)
+		{
+			actual = true;
+		}
+
+		// Assert
+		Assert.IsTrue(actual);
+	}
+
+	[Test]
+	public void ProfileBinding_InLocal()
+	{
+		// Arrange
+		var key = typeof(Test);
+		var binding = new ProfileBinding(key, null, null);
+
+		// Act
+		binding.InLocal();
+		var actual = binding.LifeTime;
+
+		// Assert
+		Assert.AreEqual(LifeTime.Local, actual);
+	}
+
+	[Test]
+	public void ProfileBinding_InLocal_IsInvalidOperationException()
+	{
+		// Arrange
+		var actual = false;
+		var key = typeof(Test);
+		var binding = new ProfileBinding(key, null, null);
+		binding.InLocal();
+
+		// Act
+		try
+		{
+			binding.InLocal();
+		}
+		catch (InvalidOperationException)
+		{
+			actual = true;
+		}
+
+		// Assert
+		Assert.IsTrue(actual);
+	}
+
+	#endregion
+
+	#region To
+
+	[Test]
+	public void ProfileBinding_ToGeneric_NotLifeTimeException()
+	{
+		// Arrange
+		var actual = false;
+		var key = typeof(Test);
+		var binding = new ProfileBinding(key, null, null);
+
+		// Act
+		try
+		{
+			binding.To<Test>();
+		}
+		catch (InvalidOperationException)
+		{
+			actual = true;
+		}
+
+		// Assert
+		Assert.IsTrue(actual);
+	}
+
+	[Test]
+	public void ProfileBinding_ToGeneric_ToGeneric_Exception()
+	{
+		// Arrange
+		var actual = false;
+		var key = typeof(Test);
+		var binding = new ProfileBinding(key, null, null);
+		binding.InGlobal().To<Test>();
+
+		// Act
+		try
+		{
+			binding.To<Test>();
+		}
+		catch (InvalidOperationException)
+		{
+			actual = true;
+		}
+
+		// Assert
+		Assert.IsTrue(actual);
+	}
+
+	[Test]
+	public void ProfileBinding_ToGeneric_ReturnIDIBinder()
+	{
+		// Arrange
+		var key = typeof(Test);
+		var binding = new ProfileBinding(key, null, null);
+
+		// Act
+		var actual = binding.InGlobal().To<Test>();
+
+		// Assert
+		Assert.AreEqual(binding, actual);
+	}
+
+	[Test]
+	public void ProfileBinding_ToGeneric_Resolver()
+	{
+		// Arrange
+		var actual = false;
+		var key = typeof(Test);
+		var binding = new ProfileBinding(key, null, Resolver);
+
+		// Act
+		binding.InGlobal().To<Test>();
+
+		void Resolver(IBinding unused) => actual = true;
+
+		// Assert
+		Assert.IsTrue(actual);
+	}
+
+	#endregion
+	
+	#region Nested
+
+	private class Test : IStorageSegmentReceiver
+	{
+		#region IStorageSegmentReceiver
+		
+		public IEnumerable<IStorageSegment> GetStorageSegments()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Apply(IStorageSegment segment)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+	
+	#endregion
+}
